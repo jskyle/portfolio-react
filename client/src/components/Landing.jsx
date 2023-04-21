d2bd06e80faabf3b89e667f745258b9d4ddc1bb3
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-04-21 10:36:08 -0600

    updated about section copy

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         <div className="d-flex align-items-end justify-content-between">
           <h1 className="landing-head">Kyle</h1>
           <h2 className="landing-subhead extralight-font">
-            Full Stack Engineer
+            Front End Developer
           </h2>
         </div>
         <h1 className="landing-head">Kearney</h1>
